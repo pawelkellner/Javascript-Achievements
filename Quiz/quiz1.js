0f3fe91674af57f87f1163ba002c1365adb82659
Pawel <smrpawel@gmail.com>
2021-01-21 17:31:37 +0100

    Quiz opdracht

---
+++ Quiz/quiz1.js
@@ -0,0 +1,69 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "MONKE TOWN",
+        "imageURI": "https://i.ytimg.com/vi/wJ76z8skaj0/maxresdefault.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "MONKE EAT BANANA?",
+				"answers": [{
+						"answer": "YES!",
+						"feedback": true
+					},
+					{
+						"answer": "ALWAYS!",
+						"feedback": true
+					},
+					{
+						"answer": "BANANA!",
+						"feedback": true
+					},
+				]
+			},
+			{
+				"question": "BANANA EAT MONKE?",
+				"answers": [{
+						"answer": "NAH MEN",
+						"feedback": true
+					},
+					{
+						"answer": "GEROME SICK!",
+						"feedback": true
+					},
+					{
+						"answer": "BABABOEY",
+						"feedback": true
+					},
+					{
+						"answer": "TRUE BRO",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "BANANA!",
+				"answers": [{
+						"answer": "MONKE!",
+						"feedback": true
+					},
+					{
+						"answer": "BANANA GOOD",
+						"feedback": true
+					},
+					{
+						"answer": "EAT BANANA!",
+						"feedback": true
+					},
+					{
+						"answer": "BANANA NOT GOOD",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
---
+++ Quiz/quiz2.js
@@ -0,0 +1,45 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "GEROME TRIVIA",
+        "imageURI": "https://i.pinimg.com/564x/59/88/c7/5988c7e61eff7d897d2bad5125e3fd9e.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "IS GEROME SICK",
+				"answers": [{
+						"answer": "ALWYAS",
+						"feedback": true
+					},
+					{
+						"answer": "NAH FAM",
+						"feedback": false
+					},
+					{
+						"answer": "AI G",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "BANANA IS LIFE",
+				"answers": [{
+						"answer": "BANANA",
+						"feedback": true
+					},
+					{
+						"answer": "NO BANANA NO LIFE",
+						"feedback": true
+					},
+					{
+						"answer": "GEROME",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
